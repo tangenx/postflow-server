cce2d0b6b9628888746ff168f6f92be39a57ff57
tαngenx <tgxed@yandex.ru>
2026-03-26 16:21:42 +0300

    feat(auth): implement basic auth middleware

---
+++ lib/middlewares/auth_middleware.dart
@@ -0,0 +1,44 @@
+import 'package:drift_postgres/drift_postgres.dart';
+import 'package:shelf/shelf.dart';
+
+import '../config/app_config.dart';
+import '../services/jwt_service.dart';
+import '../utils/is_route_public.dart';
+
+Middleware authMiddleware(AppConfig config, JwtService jwtService) {
+  return (Handler handler) {
+    return (Request request) {
+      if (isRoutePublic(request.url.path)) {
+        return handler(request);
+      }
+
+      if (!config.authenticationEnabled) {
+        return handler(
+          request.change(
+            context: {
+              'user-id': UuidValue.fromString(
+                '00000000-0000-0000-0000-000000000000',
+              ),
+            },
+          ),
+        );
+      }
+
+      final authHeader = request.headers['authorization'];
+      if (authHeader == null || !authHeader.startsWith('Bearer ')) {
+        return Response.unauthorized(null);
+      }
+
+      final token = authHeader.substring(7);
+      final userId = jwtService.verifyAccessToken(token);
+
+      if (userId == null) {
+        return Response.unauthorized(null);
+      }
+
+      return handler(
+        request.change(context: {'user-id': UuidValue.fromString(userId)}),
+      );
+    };
+  };
+}
