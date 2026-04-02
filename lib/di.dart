401ccc596203b15d0eec1635fb9ef9cbf3f5f985
tαngenx <tgxed@yandex.ru>
2026-04-02 17:08:25 +0300

    feat(posts): inject dependencies

--- lib/di.dart
+++ lib/di.dart
@@ -1,5 +1,6 @@
 import 'package:dotenv/dotenv.dart';
 import 'package:get_it/get_it.dart';
+import 'package:postflow_server/handlers/media_handler.dart';
 
 import 'config/app_config.dart';
 import 'core/constants.dart';
@@ -8,9 +9,12 @@ import 'handlers/artist_handler.dart';
 import 'handlers/auth_handler.dart';
 import 'handlers/character_handler.dart';
 import 'handlers/franchise_handler.dart';
+import 'handlers/posts_handler.dart';
 import 'handlers/user_handler.dart';
 import 'services/auth_service.dart';
 import 'services/jwt_service.dart';
+import 'services/media_service.dart';
+import 'services/posts_service.dart';
 import 'services/storage/local_storage_service.dart';
 import 'services/storage/remote_storage_service.dart';
 import 'services/storage/s3_storage_service.dart';
@@ -43,6 +47,8 @@ void registerDependencies() {
   sl.registerSingleton<FranchisesDao>(
     FranchisesDao(sl.get<PostflowDatabase>()),
   );
+  sl.registerSingleton<MediaDao>(MediaDao(sl.get<PostflowDatabase>()));
+  sl.registerSingleton<PostsDao>(PostsDao(sl.get<PostflowDatabase>()));
 
   // services
   sl.registerSingleton<JwtService>(JwtService(sl.get<AppConfig>()));
@@ -63,6 +69,21 @@ void registerDependencies() {
       StorageType.remote => RemoteStorageService(),
     },
   );
+  sl.registerSingleton<MediaService>(
+    MediaService(
+      mediaDao: sl.get<MediaDao>(),
+      storageService: sl.get<StorageService>(),
+      config: sl.get<AppConfig>(),
+    ),
+  );
+  sl.registerSingleton<PostsService>(
+    PostsService(
+      postsDao: sl.get<PostsDao>(),
+      artistsDao: sl.get<ArtistsDao>(),
+      charactersDao: sl.get<CharactersDao>(),
+      db: sl.get<PostflowDatabase>(),
+    ),
+  );
 
   // handlers
   sl.registerSingleton<AuthHandler>(
@@ -76,4 +97,13 @@ void registerDependencies() {
     FranchiseHandler(sl.get<FranchisesDao>()),
   );
   sl.registerSingleton<UserHandler>(UserHandler(sl.get<UserSettingsDao>()));
+  sl.registerSingleton<PostsHandler>(PostsHandler(sl.get<PostsService>()));
+  sl.registerSingleton<MediaHandler>(
+    MediaHandler(
+      mediaService: sl.get<MediaService>(),
+      mediaDao: sl.get<MediaDao>(),
+      config: sl.get<AppConfig>(),
+      storageService: sl.get<StorageService>(),
+    ),
+  );
 }
