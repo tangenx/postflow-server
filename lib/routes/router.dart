94b2464ee547d3754afa01708a8dba9d447e062a
tαngenx <tgxed@yandex.ru>
2026-04-04 00:16:29 +0300

    feat(captions): mount all required routes

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -5,24 +5,28 @@ import '../config/app_config.dart';
 import '../di.dart';
 import '../handlers/artist_handler.dart';
 import '../handlers/auth_handler.dart';
+import '../handlers/caption_template_handler.dart';
 import '../handlers/character_handler.dart';
 import '../handlers/franchise_handler.dart';
 import '../handlers/health.dart';
 import '../handlers/media_handler.dart';
 import '../handlers/posts_handler.dart';
+import '../handlers/social_account_target_handler.dart';
 import '../handlers/user_handler.dart';
+import '../handlers/user_social_account_hander.dart';
 import '../middlewares/auth_middleware.dart';
 import '../middlewares/error_middleware.dart';
 import '../middlewares/logging_middleware.dart';
 import '../middlewares/rate_limit_middleware.dart';
 import '../services/jwt_service.dart';
 import 'auth_routes.dart';
+import 'caption_template_routes.dart';
 import 'media_routes.dart';
 import 'posts_routes.dart';
 import 'reference_routes.dart';
+import 'social_account_target_router.dart';
 import 'user_routes.dart';
 
-// TODO: require all handler classes
 Handler buildRouter() {
   final config = sl<AppConfig>();
   final jwtService = sl<JwtService>();
@@ -31,8 +35,19 @@ Handler buildRouter() {
 
   router.mount('/api/auth/', authRouter(sl.get<AuthHandler>()).call);
   router.mount('/api/user/', userRouter(sl.get<UserHandler>()).call);
-  router.mount('/api/posts', postsRouter(sl.get<PostsHandler>()).call);
-  router.mount('/api/media', mediaRouter(sl.get<MediaHandler>()).call);
+  router.mount('/api/posts/', postsRouter(sl.get<PostsHandler>()).call);
+  router.mount('/api/media/', mediaRouter(sl.get<MediaHandler>()).call);
+  router.mount(
+    '/api/caption-templates/',
+    captionTemplateRouter(sl.get<CaptionTemplateHandler>()).call,
+  );
+  router.mount(
+    '/api/social-accounts/',
+    socialRoutes(
+      sl.get<SocialAccountTargetHandler>(),
+      sl.get<UserSocialAccountHandler>(),
+    ).call,
+  );
   // router.mount('/api/schedules', handler);
   router.mount(
     '/api/',
