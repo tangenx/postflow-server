685e7aec5c1fa18dd3ba0a0ef0757beaaaceda8b
tangenx <me@tangnex.dev>
2026-04-07 04:03:36 +0300

    feat(adapter): expand publish method

--- lib/adapters/social_adapter.dart
+++ lib/adapters/social_adapter.dart
@@ -1,13 +1,20 @@
+import 'dart:typed_data';
+
 import '../database/database.dart';
 
 abstract class SocialAdapter {
+  /// avaiable values: "user", "post"
   String get slug;
 
   Future<String> publishPost({
     required String accessToken,
     required String targetId,
     required String targetType,
-    required List<MediaFile> mediaFiles,
+    required List<MediaFileWithMediaType> mediaFiles,
+
+    /// null if media is remote
+    required List<Uint8List?> mediaBytes,
+    required String? caption,
   });
 
   Future<({String externalAccountId, String screenName})> verifyToken(
